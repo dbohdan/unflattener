1319504c8ec8661ea24465b2e2e9c4c616683b77
Danyil Bohdan <danyil.bohdan@gmail.com>
2013-11-15 22:56:43 +0200

    Improved comments/docstrings and variable naming plus a new test.
    Also added: arrays_equivalent function, support for grayscale
    output in array_to_image.

--- SpriteNormalMap.py
+++ SpriteNormalMap.py
@@ -7,93 +7,155 @@ pointfive=numpy.float64(1) / 2
 
 class SpriteNormalMap(object):
     def __init__(self):
-        self.a = None
+        self.normal_data = None
 
-    def create_from_files(self, image_file_names):
+    def create_from_files(self, image_file_names, hor_base_level=pointfive,
+                                                  vert_base_level=pointfive):
+        """
+        Generate a normal map from two or more image files.
+
+        Once the files have been loaded from the disk as objects of the class
+        Image they are then processed using the create_from_images method.
+        """
         images = {}
         for image_file in image_file_names:
             if image_file_names[image_file]: # skip the Nones
                 im = Image.open(image_file_names[image_file])
                 images[image_file] = im
-                #images[image_file] = image_to_array(im)
-        self.create_from_images(images)
+        self.create_from_images(images, hor_base_level, vert_base_level)
 
-    def create_from_images(self, images, hor_level=pointfive, vert_level=pointfive):
+    def create_from_images(self, images, hor_base_level=pointfive,
+                                         vert_base_level=pointfive):
         """
-        image_file_names is a dict that should contain the keys
-        'left', 'right', 'top' and 'bottom'.
+        Generate a normal map based on two or more images.
+
+        Once the files have been loaded from the disk and converted to arrays
+        they are then processed using the create_from_images method.
         """
+
+        # Convert all Image objects we've been given to numpy arrays.
         self.images = {k: image_to_array(images[k]) for k in images}
-        print self.images
+        #print self.images
         # Check if all images are the same size
         self.imgshape = image_shape(self.images.values())
 
-        self.a = numpy.zeros((3, self.imgshape[0], self.imgshape[1]), dtype='float64')
-        #self.a[0] = self.images['right'] - self.images['left'] # x_N
-        #self.a[1] = self.images['top'] - self.images['bottom'] # y_N
-        print 'right' in self.images, 'left' in self.images, 'top' in self.images, 'bottom' in self.images
-        self.a[0] = (self.images['right'] if 'right' in self.images else hor_level) -\
-                    (self.images['left'] if 'left' in self.images else hor_level) # x_N
-        self.a[1] = (self.images['top'] if 'top' in self.images else vert_level) -\
-                    (self.images['bottom'] if 'bottom' in self.images else vert_level) # y_N
-        self.a[2] = 1.0
+        # Create an array for normal map data.
+        self.normal_data = numpy.zeros((3, self.imgshape[0],
+                                           self.imgshape[1]),
+                                       dtype='float64')
 
-        norm = self.a[0] * self.a[0] + self.a[1] * self.a[1]
-        it = numpy.nditer(self.a[2], flags=['multi_index'])
+        # The core algorithm follows. It will be explained in detail later.
+
+        self.normal_data[0] = (self.images['right'] if 'right' in self.images
+                               else hor_base_level) -\
+                              (self.images['left'] if 'left' in self.images
+                               else hor_base_level) # x_N
+        self.normal_data[1] = (self.images['top'] if 'top' in self.images
+                               else vert_base_level) -\
+                              (self.images['bottom'] if 'bottom'
+                               in self.images else vert_base_level) # y_N
+        self.normal_data[2] = 1.0
+
+        xy_vector_norms = self.normal_data[0] * self.normal_data[0] +\
+                          self.normal_data[1] * self.normal_data[1]
+
+        # Generate z_N data from (x_N, y_N).
+        #
+        it = numpy.nditer(self.normal_data[2], flags=['multi_index'])
         while not it.finished:
             #print it, it.multi_index, it[0]
-            n = norm[it.multi_index]
+            n = xy_vector_norms[it.multi_index]
             #print it.multi_index, n
             if n > 0:
-                self.a[2][it.multi_index] = numpy.sqrt(max(1 - n / numpy.sqrt(n), 0))
+                self.normal_data[2][it.multi_index] = \
+                                     numpy.sqrt(max(1 - n / numpy.sqrt(n), 0))
             it.iternext()
 
     def save_image(self, filename, depth=pointfive):
-        aim = self.a.copy()
+        """Save the normal map to the image file filename."""
+        aim = self.normal_data.copy()
+        # Transform x_N, y_N from the -1.0..1.0 range to 0.0..1.0.
         aim[0] = (aim[0] + 1) / 2
         aim[1] = (aim[1] + 1) / 2
-        # Compress z_N range.
+        # Compress z_N range. This can be done for aesthetic reasons or due to
+        # 3D engine requirements (e.g., Doom 3).
         aim[2] = aim[2] * depth + (1 - depth)
 
         normal_map = array_to_image(aim)
         normal_map.save(filename)
 
     def load_image(self, filename, depth=pointfive):
+        """Load the normal map from the image file filename.
+
+        The depth setting is explained in save_image.
+        """
         im = Image.open(filename)
-        r, g, b  = im.split()[:3] # throw away alpha
+        r, g, b  = im.split()[:3] # Throw away the alpha channel, if any.
         self.imgshape = r.size[::-1]
-        self.a = numpy.zeros((3, self.imgshape[0], self.imgshape[1]), dtype='float64')
 
-        self.a[0] = image_to_array(r)
-        self.a[1] = image_to_array(g)
-        self.a[2] = image_to_array(b)
+        self.normal_data = numpy.zeros((3, self.imgshape[0],
+                                           self.imgshape[1]),
+                                       dtype='float64')
 
-        self.a[0] = self.a[0] * 2 - 1
-        self.a[1] = self.a[1] * 2 - 1
-        self.a[2] = (self.a[2] - (1 - depth)) / depth
+        self.normal_data[0] = image_to_array(r) # x_N
+        self.normal_data[1] = image_to_array(g) # y_N
+        self.normal_data[2] = image_to_array(b) # z_N
+
+        self.normal_data[0] = self.normal_data[0] * 2 - 1 # 0..1 -> -1..1
+        self.normal_data[1] = self.normal_data[1] * 2 - 1 # 0..1 -> -1..1
+        # Account for depth compression of z_N.
+        self.normal_data[2] = (self.normal_data[2] - (1 - depth)) / depth
 
     def apply_light(self, image, light_vector):
+        """Not implemented."""
         pass
 
     def compare(self, other, depth=pointfive):
-        return (self.a - other.a).max() < 2 * depth
+        """Compares normal map data. Returns True for equivalent maps."""
+        return arrays_equivalent(self.normal_data, other.normal_data, depth)
 
 def image_to_array(im):
+    """Convert a grayscale image to a numpy array of 0.0..1.0 of float64s."""
     d = im.convert('L').getdata()
     return numpy.array(d, dtype='float64').reshape(im.size[::-1]) / 255
 
 def array_to_image(arr, alpha=None):
-    ni = range(3)
-    for i in ni:
-        ni[i] = Image.fromarray((arr[i] * 255).astype('uint8'))
-    if alpha:
-        res = Image.merge('RGBA', ni + [alpha])
-    else:
-        res = Image.merge('RGB', ni)
+    """Convert a numpy array to grayscale or RGB(A) image.
 
+    The numpy array `arr` can either have the shape (w, h) for a grayscale
+    image result or a (3, w, h) shape for an RGB image. The alpha channel
+    data in `alpha` will be applied to the resulting RGB image if supplied.
+    """
+    def make_image(arr):
+        return Image.fromarray((arr * 255).astype('uint8'))
+
+    # Produce an RGB(A) image from an array with three values per point.
+    if arr.ndim == 3:
+        ni = range(3)
+        for i in ni:
+                ni[i] = make_image(arr[i])
+        if alpha:
+                res = Image.merge('RGBA', ni + [alpha])
+        else:
+                res = Image.merge('RGB', ni)
+    # Produce a grayscale image from a 1-value array.
+    elif arr.ndim == 2:
+        res = make_image(arr)
+    else:
+        raise ValueError
     return res
 
+def arrays_equivalent(arr1, arr2, depth=pointfive):
+    """Compares normal map data array. Returns True for equivalent data.
+
+    The comparison accounts for the discretization error introduced by
+    the `depth` setting when normal maps are saved as images.
+    """
+    max_diff = (numpy.abs(arr1 - arr2)).max() * 255
+    return max_diff < max(1 / depth, 2.01)
+
 def image_shape(images):
+    """Check if all image in `images` have same shape. Returns that shape."""
     shape = None
     for image in images:
         if not shape:
@@ -102,3 +164,4 @@ def image_shape(images):
             if shape != image.shape:
                 raise ValueError
     return shape
+
--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # Copyright (C) 2013 Danyil Bohdan
 # All rights Reserved.
+import Image
 from SpriteNormalMap import SpriteNormalMap
+from SpriteNormalMap import image_to_array, array_to_image, arrays_equivalent
 
 im_files= {'left': "zombie-left.png",
            'right': "zombie-right.png",
@@ -10,11 +12,18 @@ im_files= {'left': "zombie-left.png",
 
 for i in range(8):
     depth = 1.0 / 2**i
-    print("Running tests with depth %0.4f (1 / 2^%i)" % (depth, i))
+    print("Running normal map generation test with depth %0.4f (1 / 2^%i)" %
+          (depth, i))
     sn = SpriteNormalMap()
-    sn.create(im_files)
+    sn.create_from_files(im_files)
     sn.save_image('result.png', depth)
     sn1 = SpriteNormalMap()
     sn1.load_image('result.png', depth)
     sn1.save_image('res2.png', depth)
-    assert(sn.compare(sn1))
+    assert(sn.compare(sn1, depth))
+
+print("Testing image to array to image conversion.")
+im = Image.open(im_files['left'])
+imarr1 = image_to_array(im)
+imarr2 = image_to_array(array_to_image(imarr1))
+assert(arrays_equivalent(imarr1, imarr2))
