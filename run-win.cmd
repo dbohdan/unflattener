d93704e150d0db2916b3262adb7f282b234a0109
dbohdan <dbohdan@dbohdan.com>
2017-07-23 22:02:33 +0300

    chore: Remove run.sh and run-win.cmd
    Those scripts are not worth supporting alongside the normal
    install option.

--- run-win.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem set OUTPUTFN=res-%RANDOM%%RANDOM%
-set TESTDIR=test-images
-set PYTHON=c:\python27\python.exe
-set IMAGE=robot
-set OUTPUTFN=result-%IMAGE%.png
-echo on
-%PYTHON% unflattener\unflatten.py --top %TESTDIR%\%IMAGE%-top.png --bottom %TESTDIR%\%IMAGE%-bottom.png --left %TESTDIR%\%IMAGE%-left.png --right %TESTDIR%\%IMAGE%-right.png -o %OUTPUTFN% -d 0.5
-@pause
